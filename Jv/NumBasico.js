2bf21a9938d2b37cc1fab31a29bf9474971ae3db
VHEB <vheb0316@gmail.com>
2024-10-14 14:45:08 -0300

    NumBasico

---
+++ Jv/NumBasico.js
@@ -0,0 +1,9 @@
+function operacoesBasicas(num1, num2) {
+    console.log(`${num1} + ${num2} = ${num1 + num2}`);
+    console.log(`${num1} - ${num2} = ${num1 - num2}`);
+    console.log(`${num1} x ${num2} = ${num1 * num2}`);
+    console.log(`${num1} / ${num2} = ${num1 / num2}`);
+}
+
+// Exemplo de uso:
+operacoesBasicas(4, 5);
